abbdaa1a48ed660408c6023a3ce5316f9c4722bb
ghostinchains87 <bru.miranda@duocuc.cl>
2026-04-25 17:54:35 -0400

    fix: migracion total a PostgreSQL (init.sql syntax y db Dockerfile)

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,5 @@
-CREATE DATABASE IF NOT EXISTS tienda_perritos;
-USE tienda_perritos;
-
 CREATE TABLE IF NOT EXISTS productos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     descripcion VARCHAR(255),
     precio DECIMAL(10,2) NOT NULL,
